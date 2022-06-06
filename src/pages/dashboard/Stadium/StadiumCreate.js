c977a1e7684bbcb584c1b19170f8559f7d5d9b44
hungnln <nguyenhung09082000@yahoo.com>
2022-06-06 11:40:16 +0700

    implement stadium

---
+++ src/pages/dashboard/Stadium/StadiumCreate.js
@@ -0,0 +1,49 @@
+import { useEffect } from 'react';
+import { paramCase } from 'change-case';
+import { useParams, useLocation } from 'react-router-dom';
+// material
+import { Container } from '@mui/material';
+// redux
+import { useDispatch, useSelector } from '../../../redux/store';
+// routes
+import { PATH_DASHBOARD } from '../../../routes/paths';
+// hooks
+import useSettings from '../../../hooks/useSettings';
+// components
+import Page from '../../../components/Page';
+import HeaderBreadcrumbs from '../../../components/HeaderBreadcrumbs';
+import { getStadiumList } from 'src/redux/slices/stadium';
+import StadiumNewForm from 'src/components/_dashboard/stadium/StadiumNewForm';
+
+// ----------------------------------------------------------------------
+
+export default function StadiumCreate() {
+  const { themeStretch } = useSettings();
+  const dispatch = useDispatch();
+  const { pathname } = useLocation();
+  const { id } = useParams();
+  const { stadiumList } = useSelector((state) => state.stadium);
+  const isEdit = pathname.includes('edit');
+  const currentStadium = stadiumList.find((stadium) => stadium.id === Number(id));
+
+  useEffect(() => {
+    dispatch(getStadiumList());
+  }, [dispatch]);
+
+  return (
+    <Page title="Stadium: Create a new stadium | V League">
+      <Container maxWidth={themeStretch ? false : 'lg'}>
+        <HeaderBreadcrumbs
+          heading={!isEdit ? 'Create a new stadium' : 'Edit stadium'}
+          links={[
+            { name: 'Dashboard', href: PATH_DASHBOARD.root },
+            { name: 'Stadium', href: PATH_DASHBOARD.stadium.root },
+            { name: !isEdit ? 'New stadium' : currentStadium?.name }
+          ]}
+        />
+
+        <StadiumNewForm isEdit={isEdit} currentStadium={currentStadium} />
+      </Container>
+    </Page>
+  );
+}
